26b9712f61d77cf84019a7533e34729aa30d89ef
FoxSinOfGreed1729 <70275323+FoxSinOfGreed1729@users.noreply.github.com>
2021-06-17 10:35:26 +0530

    Crawls website for directories and subdomains

--- crawler.py
+++ crawler.py
@@ -1,3 +1,5 @@
+# Made by FoxSinOfGreed1729
+# Many thanks to Zaid Sabih and Udemy.com
 import requests
 
 
@@ -37,31 +39,28 @@ def find_directories(wordlist, target_url):
 
 
 def find_directories_recursive(content, target_url, n):
-    test_url = ""
     i = 0
     while i < n:
         word = content[i].strip()
-        # print("Word\t" + word)
         test_url = target_url + "/" + word
         req_response = request_url(test_url)
         if req_response:
             print("[+] Discovered Directory --> " + test_url)
-            # if test_url[-1] == ".":
-            #     print("breaking")
-            #     break
             find_directories_recursive(content, test_url, n)
         else:
             pass
         i = i + 1
-    # print("exiting function from " + test_url)
 
 
-target = "127.0.0.1"
-filename = "sw.txt"
-count = file_len(filename)
-with open(filename, "r") as wordlist_file:
-  file_content = wordlist_file.readlines()
-  find_directories_recursive(file_content, target, count)
-  find_directories(wordlist_file, target)
-  find_subdomains(wordlist_file, target)
-  
+def crawl(filename, target):
+    count = file_len(filename)
+    with open(filename, "r") as wordlist_file:
+        file_content = wordlist_file.readlines()
+        find_directories_recursive(file_content, target, count)
+        find_subdomains(filename, target)
+        find_directories(wordlist_file, target)
+
+
+the_target_url = "127.0.0.1"
+word_list = "sws.txt"
+crawl(word_list, the_target_url)
